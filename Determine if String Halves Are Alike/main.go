14b0a7056ab238fa994603759b3a1dc9b5a46987
jaskierrr <mr.xaski.2002@gmail.com>
2024-08-18 19:01:54 +0700

    fix Determine if String Halves Are Alike

--- Determine if String Halves Are Alike/main.go
+++ Determine if String Halves Are Alike/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 const (
-	str string = "textbook"
+	str string = "book"
 )
 
 func main() {
@@ -11,35 +11,20 @@ func main() {
 }
 
 func halvesAreAlike(s string) bool {
-	vowels := []string{"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
-	
-	vowelsMap := make(map[string]bool, len(vowels))
-	for _, v := range vowels {
-		vowelsMap[v] = true
-	}
+	vowelsMap := map[string]struct{}{"a": {}, "e": {}, "i": {}, "o": {}, "u": {}, "A": {}, "E": {}, "I": {}, "O": {}, "U": {}}
 
-	firstCount := 0
-	secondCount := 0
+	firstCount, secondCount := 0, 0
 
-	first := make([]rune, 0, len(vowels)/2)
-	second := make([]rune, 0, len(vowels)/2)
+	first, second := []rune(s[:len(s)/2]), []rune(s[len(s)/2:])
 
-	first = append(first, []rune(s[:len(s)/2])...)
-	second = append(second, []rune(s[len(s)/2:])...)
-
-	for _, v := range first {
-		if _, ok := vowelsMap[string(v)]; ok {
+	for i := 0; i < len(s)/2; i++ {
+		if _, ok := vowelsMap[string(first[i])]; ok {
 			firstCount++
 		}
-	}
-	for _, v := range second {
-		if _, ok := vowelsMap[string(v)]; ok {
+		if _, ok := vowelsMap[string(second[i])]; ok {
 			secondCount++
 		}
+
 	}
-
-	fmt.Println(first, second)
-	fmt.Println(firstCount, secondCount)
-
 	return firstCount == secondCount
 }
